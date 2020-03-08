c1640907c9d568b5351240b54f8946f2c92acbcf
Fahad Siddiqi <fahad.a.sidd@gmail.com>
2020-03-08 13:15:06 -0500

    added zsh aliases

---
+++ .aliases.zsh
@@ -0,0 +1,31 @@
+
+#General aliases
+alias ls='ls -G'
+alias c='clear'
+alias cd..='cd ../'                         # Go back 1 directory level (for fast typers)
+alias ..='cd ../'                           # Go back 1 directory level
+alias .2='cd ../../'                       # Go back 2 directory levels
+alias .3='cd ../../../'                     # Go back 3 directory levels
+alias .4='cd ../../../../'                  # Go back 4 directory levels
+alias .5='cd ../../../../../'               # Go back 5 directory levels
+alias .6='cd ../../../../../../'            # Go back 6 directory levels
+alias lsl='ls -l'
+alias lsa='ls -a'
+alias vim='/usr/local/bin/vim'
+
+#System Alliases
+alias shutdown='sudo shutdown -h now'
+alias halt='sudo halt'
+alias reboot='sudo reboot'
+
+#Directory Alliases
+alias report='open build/reports/jacoco/test/html/index.html'
+alias sdp='cd ~/homeworks/COSC\ 4354/wednesday'
+#Tmux Aliases
+alias tls='tmux ls'
+alias tmux='tmux -2'
+#VS Code alias
+alias code='open -a Visual\ Studio\ Code'
+
+alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+
